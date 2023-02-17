cdfcba939277eedfb0be7ce7bbc68b75a4e33619
TrongNhanDam <nhanb1910113@student.ctu.edu.vn>
2023-02-17 10:20:37 +0700

    Cai dat xu li loi

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express, { Router } from "express";
 import cors from "cors";
 
 import initContactRouter from "./app/router/contact.router";
+import apiError from "./app/api-error";
 
 const app = express();
 
@@ -18,4 +19,13 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcomte to project's nhan" });
 });
 
+app.use((req, res, next) => {
+  return next(new apiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res
+    .status(err.statusCode || 500)
+    .json({ message: err.message || "Internal Server Error" });
+});
 export default app;
